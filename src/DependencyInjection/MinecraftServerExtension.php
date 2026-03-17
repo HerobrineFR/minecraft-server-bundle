f67b6bda5f128a1591e158163d89177cf7296414
Yazor <yazor@herobrine.fr>
2026-03-16 20:38:42 -0400

    init bundle

---
+++ src/DependencyInjection/MinecraftServerExtension.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class MinecraftServerExtension extends Extension
+{
+
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+
+    }
+}
